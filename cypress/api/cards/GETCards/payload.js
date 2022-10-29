e098d30c6a63b5b50ac8ecd7c7af41b8831dbd1f
Jonatas Passos <jpc080496@gmail.com>
2022-10-29 16:16:20 -0300

    feat: criado request para GET Cards

--- cypress/api/cards/GETCards/payload.js
+++ cypress/api/cards/GETCards/payload.js
@@ -1,4 +1,13 @@
-export const successfulDataMass = (token) => ({
-    'Authorization': `Bearer ${token}`,
+export const successfulDataMass = () => ({
+    'Authorization': `Bearer ${Cypress.env('token')}`,
     'X-DeviceId': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
 })
+
+export const badDataMass = () => ({
+    'Authorization': `Bearer ${Cypress.env('token')}`
+})
+
+export const unauthorizedDataMass = {
+    'Authorization': 'Bearer xablau',
+    'X-DeviceId': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+}
